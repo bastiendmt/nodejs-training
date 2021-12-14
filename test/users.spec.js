ab228f260e07808b2451cdc332048e47f889da1e
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-12-14 15:07:54 +0100

    test

--- test/users.spec.js
+++ test/users.spec.js
@@ -1,8 +1,16 @@
 const supertest = require("supertest");
 const app = require("../server");
 
-test("Getting a user's posts", async () => {
-  const res = await supertest(app).get("/api/users/1/posts");
-  expect(res.status).toBe(200);
-  expect(res.body.length).toBeGreaterThan(0);
+describe("Users", () => {
+  test("Getting a user's posts", async () => {
+    const res = await supertest(app).get("/api/users/1/posts");
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+
+  test("Getting posts from a non existant user=", async () => {
+    const res = await supertest(app).get("/api/users/0/posts");
+    expect(res.status).toBe(404);
+    expect(res.body).toMatchObject({});
+  });
 });
