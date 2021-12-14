f85a9d3fb1b588a4d538d69d050eed02714c0621
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-12-14 15:58:27 +0100

    unknown routes

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const postsRouter = require("./api/posts/posts.router");
 const usersRouter = require("./api/users/users.router");
+const { NotFoundError } = require("./errors");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "./templates");
 
 app.use(cors());
 app.use(express.json()); // This middleware can also be placed in a specific route
@@ -20,10 +24,15 @@ app.use("/error", (req, res, next) => {
   next(new Error("something went wrong"));
 });
 
+// Catches unknown routes
+app.use((req, res, next) => {
+  next(new NotFoundError("Unknown route"));
+});
+
 // Error handler. Used to log errors or easily add
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(err.status || 500).send(err.message);
+  res.status(err.status || 500).render("error");
 });
 
 module.exports = app;
