db963c16b09cf95a60864f0d5b23c7582b7ac453
Bastien Dumont <32489032+bastiendmt@users.noreply.github.com>
2021-12-14 16:34:58 +0100

    security handle

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const postsRouter = require("./api/posts/posts.router");
 const usersRouter = require("./api/users/users.router");
 const { NotFoundError } = require("./errors");
@@ -12,6 +13,7 @@ app.set("views", "./templates");
 
 app.use(cors());
 app.use(express.json()); // This middleware can also be placed in a specific route
+app.use(helmet()); // This midleware handles XSS breaks and many others
 
 app.use("/api/posts", postsRouter);
 app.use("/api/users", usersRouter);
@@ -32,10 +34,10 @@ app.use((req, res, next) => {
 // Error handler. Used to log errors or easily add
 app.use((err, req, res, next) => {
   console.log(err);
-  const status = err.status || 500
+  const status = err.status || 500;
   res.status(status).render("error", {
     status,
-    message: err.message
+    message: err.message,
   });
 });
 
